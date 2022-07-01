a0f82a525463ecd5c76af114c8be95e9732854e7
Alexandre Moreira <brainmunchers@gmail.com>
2022-07-01 11:44:55 +0200

    add mobile image

--- components/BGImage.tsx
+++ components/BGImage.tsx
@@ -1,34 +1,42 @@
 import Image from 'next/image';
+import useAppDimensions from '../hooks/useAppDimensions';
 import { Box } from '@mui/system';
-
 import { useRouter } from 'next/router';
 
-import bg from '../public/images/main-page-background.png';
+import indexBGDesktop from '../public/images/main-page-background.png';
+import indexBGMobile from '../public/images/main-page-background-mobile.png';
 
 export default function BGImage() {
   const router = useRouter();
+  const { isMobile } = useAppDimensions();
 
   console.log(router.pathname);
 
+  const BGImage = isMobile ? indexBGMobile : indexBGDesktop;
+
   return (
     <>
-      <Box
-        position="fixed"
-        left={0}
-        id="index-gradient"
-        width="50%"
-        height="100%"
-        zIndex={1}
-      />
+      {router.pathname === '/' && (
+        <Box
+          position="fixed"
+          left={0}
+          id="index-gradient"
+          width="50%"
+          height="100%"
+          zIndex={1}
+        />
+      )}
+
       <Box
         zIndex={0}
         style={{
           width: '100%',
           height: '100%',
+          filter: 'blur(0.8px)',
         }}
       >
         <Image
-          src={bg.src}
+          src={BGImage.src}
           alt="main-page-background"
           layout="fill"
           objectFit="cover"
